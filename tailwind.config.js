a92d0220b855f9007da0cf3a06ce0e65aaa49b24
Ivan Karpov <>
2022-05-27 11:54:42 +0200

    Refactoring

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,12 @@
 module.exports = {
   content: ['./src/**/*.{hbs,js}'],
   theme: {
+    fontFamily: {
+      'family-light': ['Roboto-Light', 'sans-serif'],
+      'family-regular': ['Roboto-Regular', 'sans-serif'],
+      'family-bolder': ['Roboto-Bolder', 'sans-serif'],
+      'family-bold': ['Roboto-Bold', 'sans-serif'],
+    },
     extend: {},
   },
   plugins: [],
